b3737f9ed1771ee0a461076250fd0812f388f374
Apostolos-Bizou <kagelaris1@gmail.com>
2026-04-14 20:07:47 +0300

    chore: add .gitignore, remove node_modules and temp scripts from tracking

--- fix_gantt.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-path = r'C:\Users\akage\Next2View\frontend\src\views\DashboardView.vue'
-content = open(path, encoding='utf-8').read()
-old = "    weeks.push({ label: `${d.getDate()} ${months[d.getMonth()]}`, isToday })"
-new = "    weeks.push({ num: i + 1, dateLabel: `${d.getDate()} ${months[d.getMonth()]}`, isCurrentWeek: isToday })"
-result = content.replace(old, new)
-print('Found:', old in content)
-open(path, 'w', encoding='utf-8', newline='').write(result)
-print('Done')
--- genhash.js
+++
@@ -1,5 +0,0 @@
-﻿const { execSync } = require('child_process');
-try { require('bcryptjs'); } catch(e) { execSync('npm install bcryptjs', {cwd: 'C:\\Users\\akage\\Next2View'}); }
-const bcrypt = require('bcryptjs');
-const hash = bcrypt.hashSync('Test@2026!', 12);
-console.log(hash);
