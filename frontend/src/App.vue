9450581be8a6ecc0cc6a211f37df59ef8aca0a82
Apostolos-Bizou <kagelaris1@gmail.com>
2026-04-15 10:18:43 +0300

    feat: Add PWA support with install prompt and N2V icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <router-view />
+  <PwaInstallPrompt />
 </template>
-
 <script setup>
 import { onMounted } from "vue";
 import { useAuthStore } from "@/stores/auth";
-
+import PwaInstallPrompt from "@/components/PwaInstallPrompt.vue";
 const auth = useAuthStore();
-onMounted(() => auth.init());
+onMounted(() => {
+  auth.init();
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg.scope))
+      .catch(err => console.log('SW error:', err));
+  }
+});
 </script>
